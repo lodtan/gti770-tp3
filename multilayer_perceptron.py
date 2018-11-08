0a8f9e48b17e747eeb7fa94c4dad32ce90aa3dea
Jacques-Sylvain Lecointre <jslecointre@mnubo.com>
2018-11-07 23:33:01 -0500

    fix MLP => still nan for cost values

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -31,8 +31,6 @@ def mlp_benchmark(X_train ,y_train ,X_test ,y_test ,n_hidden_1=10 ,learning_rate
     # couche cachee entierement connectee avec n_hidden_1 neurones
     print(weights['h1'])
 
-    layer_1 = tf.add(tf.matmul(X, weights['h1']), biases['b1'])
-    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
     logits = multilayer_perceptron(X)
 
     # Reformattage des vecteurs au format tensorflow
@@ -46,12 +44,12 @@ def mlp_benchmark(X_train ,y_train ,X_test ,y_test ,n_hidden_1=10 ,learning_rate
 
     # Descente de Gradient
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+
     train_op = optimizer.minimize(loss_op)
 
     # Initialisation
     init = tf.global_variables_initializer()
     epoch_vs_cost = []
-    epoch_vs_cost_valid = []
 
     # HyperParametres
     print('training_epochs : {0} learning_rate : {1} batch_size : {2}'.format(training_epochs, learning_rate, batch_size))
@@ -66,28 +64,27 @@ def mlp_benchmark(X_train ,y_train ,X_test ,y_test ,n_hidden_1=10 ,learning_rate
         # cycle d entrainement
         for epoch in range(training_epochs):
             avg_cost = 0.
-            avg_cost_valid = 0.
             total_batch = int(len(X_train) / batch_size)
 
-        # boucle pour tous les mini batches
-        for i in range(total_batch):
+            # boucle pour tous les mini batches
+            for i in range(total_batch):
 
-            batch_x = X_train[i * (batch_size):i * (batch_size) + batch_size]
+                batch_x = X_train[i * (batch_size):i * (batch_size) + batch_size]
+                batch_y = [y_vector(x) for x in y_train[i * (batch_size):i * (batch_size) + batch_size]]
 
-            batch_y = [y_vector(x) for x in y_train[i * (batch_size):i * (batch_size) + batch_size]]
+                # optimisation et backpropagation => valeurs du cout sur training
+                _, c = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y})
 
-            # optimisation et backpropagation => valeurs du cout sur training ET validation
-            _, c = sess.run([train_op, loss_op], feed_dict={X: batch_x, Y: batch_y})
-
-            # Calcul de cout moyen (ajout a chaque batch)
-            avg_cost += c / total_batch
+                # Calcul de cout moyen (ajout a chaque batch)
+                avg_cost += c / total_batch
 
             if epoch % display_step == 0:
-                print('epoch {0} cost={1}'.format(epoch + 1, avg_cost))
+                print("Epoch:", '%04d' % (epoch + 1), "cost={:.9f}".format(avg_cost))
 
             epoch_vs_cost.append([epoch + 1, avg_cost])
 
         end_time = datetime.datetime.now()
+        print("Optimization Finished!")
 
         #################### # Test du modele # ####################
         pred = tf.nn.softmax(logits)  # softmax en sortie
@@ -96,9 +93,6 @@ def mlp_benchmark(X_train ,y_train ,X_test ,y_test ,n_hidden_1=10 ,learning_rate
                                             tf.argmax(Y, 1))  # dataframes pour affichage des couts VALID VS TRAINING
 
         df1 = pd.DataFrame(epoch_vs_cost, columns=['epoch', 'cost'])
-        df1['category'] = 'train'
-        df2 = pd.DataFrame(epoch_vs_cost_valid, columns=['epoch', 'cost'])
-        df2['category'] = 'valid'
 
         # Calcul de l accuracy
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))  # Calculate de l erreur
@@ -115,7 +109,7 @@ def mlp_benchmark(X_train ,y_train ,X_test ,y_test ,n_hidden_1=10 ,learning_rate
         return {'accuracy': accuracy.eval({X: X_test, Y: [y_vector(x) for x in y_test]}),
                 'error': error.eval({X: X_test, Y: [y_vector(x) for x in y_test]}),
                 'y_hat': y_test_hat,
-                'df_epoch': pd.concat([df1, df2]),
+                'df_epoch': df1,
                 'total_time': (end_time - start_time).total_seconds()}
 
 
@@ -134,4 +128,4 @@ if __name__ == "__main__":
     X = tf.placeholder("float", [None, n_input])
     Y = tf.placeholder("float", [None, n_classes])  # poids et biais
 
-    a = mlp_benchmark(X_train, Y_train, X_test, Y_test, n_hidden_1=n_hidden_1, learning_rate=0.01, training_epochs=500,batch_size=100)
+    a = mlp_benchmark(X_train, Y_train, X_test, Y_test, n_hidden_1=n_hidden_1, learning_rate=0.1, training_epochs=2,batch_size=100)
